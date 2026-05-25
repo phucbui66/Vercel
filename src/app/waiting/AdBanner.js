47334ff98a58698e17f5cb8873bffcc7b248f3d9
Phuc Bui <phucbui66@github.com>
2026-05-25 23:35:08 +0700

    Inject actual Adsterra banner via iframe to prevent document.write issues

--- src/app/waiting/AdBanner.js
+++ src/app/waiting/AdBanner.js
@@ -1,28 +1,15 @@
-"use client";
-
-import { useEffect, useRef } from 'react';
-
 export default function AdBanner() {
-  const bannerRef = useRef(null);
-
-  useEffect(() => {
-    if (bannerRef.current && !bannerRef.current.hasChildNodes()) {
-      const script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.src = 'https://www.effectivecpmnetwork.com/zkzucecc?key=f7149d9f04f436f54a24ebcbb8b64572';
-      script.async = true;
-      
-      // Chèn script vào trong thẻ div để quảng cáo hiển thị ngay tại đây
-      bannerRef.current.appendChild(script);
-    }
-  }, []);
-
   return (
-    <div 
-      ref={bannerRef} 
-      className="w-full flex items-center justify-center min-h-[90px] overflow-hidden"
-    >
-      {/* Script sẽ tự động sinh HTML quảng cáo vào bên trong div này */}
+    <div className="w-full flex items-center justify-center min-h-[50px] overflow-hidden">
+      <iframe 
+        src="/ad.html" 
+        width="320" 
+        height="50" 
+        frameBorder="0" 
+        scrolling="no" 
+        className="border-none"
+        title="Advertisement"
+      ></iframe>
     </div>
   );
 }
